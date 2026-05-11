33cfac6b032ed0f315e4c5f1d66c843e268a9f99
Thalia dos Santos Silva <thaliasilva07@hotmail.com>
2026-05-10 23:18:02 -0300

    staging dbt config

--- dbt_project/models/staging/staging_censo_escolar.sql
+++ dbt_project/models/staging/staging_censo_escolar.sql
@@ -1,1 +1,11 @@
-SELECT * FROM {{ source("minio_raw", "censo_trusted_parquet") }}
+{{ config(materialized='view') }}
+
+with raw_data as (
+    -- Esta parte traz os dados brutos do seu MinIO
+    select * from {{ source('minio_raw', 'censo_trusted_parquet') }}
+)
+
+select
+    -- Aqui você pode filtrar ou renomear colunas no futuro
+    *
+from raw_data
